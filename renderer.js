b84cfb670d4f2017463433278889d636ea5e6302
Leonardo <leonardogarcarr23@gmail.com>
2025-02-14 14:34:16 -0500

    new fonts

--- renderer.js
+++ renderer.js
@@ -2,29 +2,32 @@ const dropZone = document.getElementById('dropZone');
 const audioPlayer = document.getElementById('audioPlayer');
 const songName = document.getElementById('songName');
 
-// Manejar eventos de arrastre
 dropZone.addEventListener('dragover', (event) => {
-  event.preventDefault(); // Prevenir el comportamiento predeterminado
-  dropZone.style.borderColor = '#007bff'; // Cambiar el color del borde
+  event.preventDefault();
+  dropZone.style.borderColor = '#007bff';
 });
 
 dropZone.addEventListener('dragleave', () => {
-  dropZone.style.borderColor = '#ccc'; // Restaurar el color del borde
+  dropZone.style.borderColor = '#ccc';
 });
 
 dropZone.addEventListener('drop', (event) => {
   event.preventDefault();
-  dropZone.style.borderColor = '#ccc'; // Restaurar el color del borde
+  dropZone.style.borderColor = '#ccc';
 
-  // Obtener el archivo arrastrado
   const files = event.dataTransfer.files;
+
   if (files.length > 0) {
-    const file = files[0]; // Tomamos el primer archivo
+
+    const file = files[0];
+
     if (file.type.startsWith('audio/')) {
-      const filePath = URL.createObjectURL(file); // Crear una URL para el archivo
+
+      const filePath = URL.createObjectURL(file);
       this.songName.textContent = file.name;
-      audioPlayer.src = filePath; // Asignar la ruta al reproductor
-      audioPlayer.play(); // Reproducir automáticamente
+      audioPlayer.src = filePath;
+      audioPlayer.play();
+
     } else {
       alert('Por favor, arrastra un archivo de audio válido.');
     }
